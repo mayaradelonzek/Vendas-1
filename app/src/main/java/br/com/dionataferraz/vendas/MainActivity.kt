3b3870d0551b45ebc20473e168834f2b4eb45a1a
Dionata Ferraz <dionata.ferraz@ifood.com.br>
2022-08-04 01:40:54 -0300

    feat: Refactoring to use viewbinging

--- app/src/main/java/br/com/dionataferraz/vendas/MainActivity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/MainActivity.kt
@@ -2,68 +2,60 @@ package br.com.dionataferraz.vendas
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import br.com.dionataferraz.vendas.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btOk: Button
-    private lateinit var btClear: Button
-    private lateinit var etName: EditText
-    private lateinit var tvAnswer: TextView
-    private lateinit var container: ConstraintLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        initializeViews()
         initializeBtOkListener()
         initializeBtClearListener()
     }
 
-    private fun initializeViews() {
-        btOk = findViewById(R.id.bt_ok)
-        btClear = findViewById(R.id.bt_clear)
-        etName = findViewById(R.id.et_name)
-        tvAnswer = findViewById(R.id.tv_answer)
-        container = findViewById(R.id.container)
-    }
-
     private fun initializeBtOkListener() {
-        btOk.setOnClickListener {
-            if (!etName.text.isNullOrBlank()) {
-                tvAnswer.text = getString(R.string.hello, etName.text)
-                tvAnswer.visibility = View.VISIBLE
-            } else {
-                showMessage()
+        binding.run {
+            btOk.setOnClickListener {
+                if (!etName.text.isNullOrBlank()) {
+                    tvAnswer.text = getString(R.string.hello, etName.text)
+                    tvAnswer.visibility = View.VISIBLE
+                } else {
+                    showMessage()
+                }
             }
         }
     }
 
     private fun initializeBtClearListener() {
-        btClear.setOnClickListener {
-            tvAnswer.text = ""
-            etName.text.clear()
-            tvAnswer.visibility = View.GONE
+        binding.run {
+            btClear.setOnClickListener {
+                tvAnswer.text = ""
+                etName.text.clear()
+                tvAnswer.visibility = View.GONE
+            }
         }
     }
 
     private fun showMessage() {
-        Snackbar.make(
-            container,
-            getString(R.string.empty_name),
-            Snackbar.LENGTH_LONG
-        ).setAction("Tente novamente") {
-            Toast.makeText(
-                this,
-                "Sou um toast", Toast.LENGTH_LONG
-            ).show()
-        }.show()
+        binding.run {
+            Snackbar.make(
+                container,
+                getString(R.string.empty_name),
+                Snackbar.LENGTH_LONG
+            ).setAction("Tente novamente") {
+                Toast.makeText(
+                    applicationContext,
+                    "Sou um toast", Toast.LENGTH_LONG
+                ).show()
+            }.show()
+        }
     }
 }
