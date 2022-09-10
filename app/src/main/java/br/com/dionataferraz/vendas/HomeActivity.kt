66b4248338bd1dff4d8e759eb91d47240db5e974
Mayara Delonzek <mayaradelonzek2@gmail.com>
2022-09-10 15:48:41 -0300

    fragment init

--- app/src/main/java/br/com/dionataferraz/vendas/HomeActivity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/HomeActivity.kt
@@ -3,6 +3,9 @@ package br.com.dionataferraz.vendas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import br.com.dionataferraz.vendas.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -23,4 +26,19 @@ class HomeActivity : AppCompatActivity() {
         }
 
     }
+}
+
+class ViewPagerAdapter(fm: FragmentManager) :
+    FragmentStatePagerAdapter(fm) {
+
+    override fun getCount(): Int = 3
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "Tab ${position.plus(1)}"
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return ConatinerFragment.newInstance("${position.plus(1)}")
+    }
+
 }
